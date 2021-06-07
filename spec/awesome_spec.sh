23938fc13461af3426605e0a3ed1f88494fc6252
Shinichi Okada <okada.shin@gmail.com>
2021-06-08 08:05:44 +0900

    fn_repo_script test added

--- spec/awesome_spec.sh
+++ spec/awesome_spec.sh
@@ -1,22 +1,15 @@
 #shellcheck shell=sh
 
-Describe 'this is "example group"'
-    Context 'this is also "example group"'
-        # You can write an "example" here
+Describe 'run fn_repo_script()'
+    Include ./lib
+    It 'outputs git_user, repo_name, script_name, repo_link'
+        When call fn_repo_script shinokada/gitstart
+        The line 1 of output should eq 'shinokada gitstart gitstart https://github.com/shinokada/gitstart'
+        #     The line 2 of output should eq 'tldr'
     End
-
-    Describe '"example group" can be nestable'
-        # You can write an "example" here
+    It 'outputs git_user, repo_name, script_name, repo_link'
+        When call fn_repo_script raylee/tldr-sh-client tldr
+        The line 1 of output should eq 'raylee tldr-sh-client tldr https://github.com/raylee/tldr-sh-client'
+        #     The line 2 of output should eq 'tldr'
     End
-
-    Context 'this is also "example group"'
-        # You can write an "example" here
-
-        Describe '"example group" can be nestable'
-            # You can write an "example" here
-        End
-
-        # You can write an "example" here
-    End
-    # You can write an "example" here
 End
