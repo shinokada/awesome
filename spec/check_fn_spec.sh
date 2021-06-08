7d4b8a24dfdba711b698a7315f4cbb3a27b821c2
Shinichi Okada <okada.shin@gmail.com>
2021-06-08 15:58:03 +0900

    Test src_path() is done

--- spec/check_fn_spec.sh
+++ spec/check_fn_spec.sh
@@ -11,6 +11,7 @@ Describe 'Tests for check functions.'
         # since checkOrmkdir run mkdir $1, you need to remove from the script dir
         rm -rf "$dirname"
     }
+
     Describe 'Testing checkOrmkdir()'
         It 'outputs the status of 1.'
             When run checkOrmkdir
@@ -42,23 +43,7 @@ Describe 'Tests for check functions.'
         End
     End
 
-    Describe 'Testing fn_list().'
-        # setup() {
-        #     touch "$file"
-        #     ln -s "$file" "$link"
-        # }
-        # cleanup() {
-        #     return
-        #     rm -rf "$file"
-        #     rm "$link"
-        # }
-        # It 'outputs the status of 1.'
-        #     When run fn_list "$dirname"
-        #     The value "$repo_name" should eq 'gitstart'
-        # End
-    End
-
-    Describe 'Testing check_link().'
+    Describe 'Tests for list and link'
         filename=$(openssl rand -hex 4)
         awesome_dir="/tmp/awesome"
         file="$awesome_dir/$filename"
@@ -73,13 +58,30 @@ Describe 'Tests for check functions.'
         cleanup() {
             rm -rf "$bin_dir"
             rm -rf "$awesome_dir"
-            # rm "$link"
         }
-        Before 'setup'
-        After 'cleanup'
-        It 'outputs the status of 0.'
-            When run check_link "my-link" "$bin_dir"
-            The status should eq 0
+        BeforeAll 'setup'
+        AfterAll 'cleanup'
+
+        Describe 'Testing fn_list().'
+            It 'outputs the my-link.'
+                When run fn_list "$bin_dir"
+                The stdout should eq "my-link"
+                The status should eq 0
+            End
+        End
+
+        Describe 'Testing check_link().'
+            It 'outputs the status of 0.'
+                When run check_link "my-link" "$bin_dir"
+                The status should eq 0
+            End
+        End
+
+        Describe 'Testing src_path().'
+            It 'outputs source path.'
+                When run src_path "$(realpath "$link")"
+                The stdout should eq "/private${awesome_dir}"
+            End
         End
     End
 End
