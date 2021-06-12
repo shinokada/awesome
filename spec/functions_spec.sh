17ec77cb14dc544c4a83051a774be88258a44e82
Shinichi Okada <okada.shin@gmail.com>
2021-06-12 17:32:48 +0900

    Work on progress. uninstall

--- spec/functions_spec.sh
+++ spec/functions_spec.sh
@@ -12,7 +12,7 @@ Describe 'Tests for functions.'
         rm -rf "$dirname"
     }
 
-    Describe 'Testing checkOrmkdir()'
+    Describe 'Testing checkOrmkdir'
         It 'outputs the status of 1.'
             When run checkOrmkdir
             The stderr should eq "Specify the directory."
@@ -61,7 +61,7 @@ Describe 'Tests for functions.'
         BeforeAll 'setup'
         AfterAll 'cleanup'
 
-        Describe 'Testing symlink_names().'
+        Describe 'Testing symlink_names.'
             It 'outputs the my-link.'
                 When run symlink_names "$bin_dir"
                 The stdout should eq "my-link"
@@ -69,28 +69,28 @@ Describe 'Tests for functions.'
             End
         End
 
-        Describe 'Testing no_symlink().'
+        Describe 'Testing no_symlink.'
             It 'outputs the status of 0.'
                 When run no_symlink "no-link" "$bin_dir"
                 The status should eq 0
             End
         End
 
-        Describe 'Testing no_symlink().'
+        Describe 'Testing no_symlink.'
             It 'outputs the status of 1.'
                 When run no_symlink "my-link" "$bin_dir"
                 The status should eq 1
             End
         End
 
-        Describe 'Testing src_path().'
+        Describe 'Testing src_path.'
             It 'outputs source path.'
                 When run src_path "$(realpath "$link")"
                 The stdout should eq "/private${awesome_dir}"
             End
         End
 
-        Describe 'Testing check_cmd()'
+        Describe 'Testing check_cmd'
             It 'uses command -v  and outputs 0 or 1 depending a command exists or not'
                 When run check_cmd cd
                 The status should eq 0
@@ -101,9 +101,30 @@ Describe 'Tests for functions.'
                 The stderr should eq "Please install abcdefg"
             End
         End
+        Describe 'Testing checkFile.'
+            It 'outputs file 0 if there is a file.'
+                When run checkFile "dir/file"
+                The status should eq 1
+            End
+        End
+
+        Describe 'Testing checkFile.'
+            It 'outputs file 0 if there is a file.'
+                When run checkFile "$file"
+                The status should eq 0
+            End
+        End
+
+        Describe 'Testing num_symlinks.'
+            It 'outputs 1 when there is a one symlink.'
+                When run num_symlinks "$filename" "$bin_dir"
+                The output should eq 1
+            End
+        End
+
     End
 
-    Describe 'Testing slash_num()'
+    Describe 'Testing slash_num'
         It 'outputs 0.'
             When run slashes "path"
             The stdout should eq "0"
@@ -122,10 +143,24 @@ Describe 'Tests for functions.'
         End
     End
 
-    Describe 'Testing check_branch().'
-        It 'outputs master.'
-            When run check_branch "$(realpath "$link")"
-            The stdout should eq "/private${awesome_dir}"
+    Describe 'Testing first.'
+        It 'outputs first part of a path.'
+            When run first "dir/file"
+            The stdout should eq "dir"
         End
     End
+    Describe 'Testing second.'
+        It 'outputs second part of a path.'
+            When run second "dir/file"
+            The stdout should eq "file"
+        End
+    End
+
+    Describe 'Testing main_repo.'
+        It 'outputs second part of a path.'
+            When run main_repo "dir/file"
+            The stdout should eq "file"
+        End
+    End
+
 End
